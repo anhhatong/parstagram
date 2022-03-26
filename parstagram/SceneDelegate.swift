d2ce3806a00dd9e11d31f1b0c71e9584c633e167
anh.tong <anh.tong@kyanon.digital>
2022-03-26 13:13:43 -0400

    Implemented login persist

--- parstagram/SceneDelegate.swift
+++ parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil {
+            // Look for file named Main.storyboard
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            // Look for navigation controller named FeedNavigationController in Main storyboard
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            // set root view to feed page
+            window?.rootViewController = feedNavigationController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
